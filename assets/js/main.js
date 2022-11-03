081a45237553449bda60729faf049e0a4b5065be
undo <undo@idrsystems.co.kr>
2022-11-03 14:23:34 +0900

    home page작업진행중 / 다크모드 test

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,14 @@
 "use strict";
+
+window.onload = function(){
+
+const $checkbox = document.querySelector('.check');
+
+$checkbox.addEventListener('click', e=> {
+  if (e.target.checked) {
+    document.documentElement.setAttribute('color-theme', 'dark');
+  } else {
+    document.documentElement.setAttribute('color-theme', 'light');
+  }
+});
+}
