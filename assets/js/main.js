1bbc2c68a4d96f53c89225ca8f108dff60449ecd
undo <undo@idrsystems.co.kr>
2022-11-04 16:25:00 +0900

    home 코딩진행중

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,28 @@ $checkbox.addEventListener('click', e=> {
     document.documentElement.setAttribute('color-theme', 'light');
   }
 });
+
+// var swiper = new Swiper(".mySwiper", {
+//   slidesPerView: 1,
+//   spaceBetween: 10,
+//   loop: true,
+//   autoplay: {
+//     delay: 2500
+//   },
+//   breakpoints: {
+//     640: {
+//       slidesPerView: 2,
+//       spaceBetween: 20
+//     },
+//     768: {
+//       slidesPerView: 4,
+//       spaceBetween: 40
+//     },
+//     1024: {
+//       slidesPerView: 5,
+//       spaceBetween: 50
+//     }
+//   }
+// });
+
+
